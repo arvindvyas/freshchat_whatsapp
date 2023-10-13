736962308aa3e5d3b063bf56a050ca79b76b63ca
Arvind Vyas <arvindvyas07@gmail.com>
2023-10-13 19:08:02 +0530

    Added  faraday for json api calls also setup the cleint to call the api

--- lib/freshchat_whatsapp/client.rb
+++ lib/freshchat_whatsapp/client.rb
@@ -1,7 +1,44 @@
 # frozen_string_literal: true
-
 module FreshchatWhatsapp
   class Client
-    
+    def initialize(token = nil, token_type = :bearer)
+      @base_path = FreshchatWhatsapp.configuration.base_path
+      @token = token
+      @token_type = token_type
+    end
+
+    def request(path, payload = nil)
+      full_path = "#{base_path}#{path}"
+
+      conn = Faraday.new(url: full_path, headers: headers)
+      response = conn.post { |request| request.body = body(payload) }
+
+      raise FreshchatWhatsapp::Errors::RequestError.new("FreshchatWhatsapp API request error.", response) unless response.success?
+
+      JSON.parse(response.body)
+    end
+
+    private
+
+    attr_reader :base_path, :token, :token_type
+
+    def token_name
+      case token_type
+      when :bearer
+        "Bearer"
+      end
+    end
+
+   
+    def headers
+      {
+        "Authorization" => "#{token_name} #{token}",
+        "Content-Type" => "application/json",
+      }
+    end
+
+    def body(payload)
+      payload && payload.to_json
+    end
   end
 end
