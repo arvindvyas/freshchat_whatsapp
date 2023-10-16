8289172781db73046d53fb8fb71d2d1b5e0bce75
Arvind Vyas <arvindvyas07@gmail.com>
2023-10-16 20:27:51 +0530

    Added  method to api call

--- lib/freshchat_whatsapp/api.rb
+++ lib/freshchat_whatsapp/api.rb
@@ -5,6 +5,23 @@ require "freshchat_whatsapp/actions/send_message"
 module FreshchatWhatsapp
   class Api
     def initialize
+      @base_path = FreshchatWhatsapp.configuration.base_path
+    end
+
+  
+
+    def send_message(username, body)
+      Actions::SendMessage.new(client, body).call
+    end
+
+    def send_hsm_message(username, namespace, element_name, language, params)
+      Actions::SendHsmMessage.new(
+        client,
+        namespace,
+        element_name,
+        language,
+        params
+      ).call
     end
 
     private
@@ -12,7 +29,7 @@ module FreshchatWhatsapp
     attr_reader :base_path
 
     def client
-     
+      @client ||= FreshchatWhatsapp::Client.new
     end
   end
 end
