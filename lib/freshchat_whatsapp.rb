ffcdd05ea571f8f2ac52debcba432b869a88aea7
Arvind Vyas <arvindvyas07@gmail.com>
2023-11-01 23:16:27 +0530

    [FIX] rearrange the required file added payload to send sms

--- lib/freshchat_whatsapp.rb
+++ lib/freshchat_whatsapp.rb
@@ -1,12 +1,13 @@
+# frozen_string_literal: true
 
 # Dependencies
-require "json"
-require "faraday"
+require 'json'
+require 'faraday'
 
 # Source
-require "freshchat_whatsapp/configuration"
-require "freshchat_whatsapp/version"
-require "freshchat_whatsapp/actions/send_hsm_message"
-require "freshchat_whatsapp/errors/request_error"
-require "freshchat_whatsapp/client"
-require "freshchat_whatsapp/api"
+require 'freshchat_whatsapp/configuration'
+require 'freshchat_whatsapp/version'
+require 'freshchat_whatsapp/actions/send_hsm_message'
+require 'freshchat_whatsapp/errors/request_error'
+require 'freshchat_whatsapp/client'
+require 'freshchat_whatsapp/api'
--- lib/freshchat_whatsapp/actions/send_hsm_message.rb
+++ lib/freshchat_whatsapp/actions/send_hsm_message.rb
@@ -3,6 +3,66 @@
 module FreshchatWhatsapp
   module Actions
     class SendHsmMessage
+      PATH = '/v2/outbound-messages/whatsapp'
+
+      def initialize(client, wtsapp_phone_number, namespace, template_name, language, params)
+        @phone_number = wtsapp_phone_number
+        @namespace = namespace
+        @template_name = template_name
+        @language = language
+        @params = params
+        @client = client
+      end
+
+      def call
+        client.request PATH, payload
+      end
+
+      private
+
+      attr_reader :client, :wtsapp_phone_number, :namespace, :template_name, :language, :params
+
+      def payload
+        payload = {
+          'from': {
+            'phone_number': @from_number
+          },
+          'to': [
+            {
+              'phone_number': @phone_number
+            }
+          ],
+          'data': {
+            'message_template': {
+              'storage': 'conversation',
+              'namespace': @namespace,
+              'template_name': @template_name,
+              'language': {
+                'policy': 'deterministic',
+                'code': 'en'
+              },
+              'rich_template_data': {
+                'button': {
+                  'subType': 'url',
+                  'params': [
+                    {
+                      'data': params[:url]
+                    }
+                  ]
+                },
+                'body': {
+                  'params': [
+                    {
+                      'data': params[:name]
+                    }
+                  ]
+                }
+              }
+            }
+          }
+        }
+        payload
+      end
     end
   end
 end
